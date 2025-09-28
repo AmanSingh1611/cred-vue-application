3962f7c7f1455ca90c0b81e07ef86a63b3faf327
Aman Singh <aman16024375@gmail.com>
2025-09-28 19:58:49 +0530

    Initial Commit

---
+++ cred-vue/cred_vueApp.swift
@@ -0,0 +1,17 @@
+//
+//  cred_vueApp.swift
+//  cred-vue
+//
+//  Created by Aman Kumar Singh on 28/09/25.
+//
+
+import SwiftUI
+
+@main
+struct cred_vueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
